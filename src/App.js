429c76ee6774a8028e3c3a63eb9643150e84ac8a
Ramen96 <eric.worsfold@gmail.com>
2024-02-20 19:53:10 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
     //////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    const PAT = '3281b828c455496aad24c3a040f19464';
+    const PAT = 'YOUR_PTA_TOKEN';
     // Specify the correct user_id/app_id pairings
     // Since you're making inferences outside your app's scope
     const USER_ID = 'clarifai';
